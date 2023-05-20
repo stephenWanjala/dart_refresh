49db0f1ec37d55034a54c5971936f8fa8d269a5c
Wanjala Stephen <stephenwanjala145@gmail.com>
2023-05-20 13:56:37 +0300

    Variables and data types

---
+++ datatypesAndVariables.dart
@@ -0,0 +1,25 @@
+void main(List<String> args) {
+  
+
+  // Variable declaration
+int age = 25;
+double pi = 3.14159;
+bool isDartFun = true;
+String name = 'John';
+
+// Dynamic type
+var dynamicVariable = 'This can hold any type';
+
+// Constants
+const double gravity = 9.8;
+final String appName = 'My App';
+
+// Printing to the console
+print(age);
+print(pi);
+print(isDartFun);
+print(name);
+print(dynamicVariable);
+print(gravity);
+print(appName);
+}
