9b1a14dca819ab12f19b1e88183c8ff07dcf1928
Wanjala Stephen <stephenwanjala145@gmail.com>
2023-05-20 15:44:16 +0300

    collection litterals

---
+++ collectionsLitearls.dart
@@ -0,0 +1,19 @@
+void main(List<String> args) {
+  // List literal
+  var numbers = [1, 2, 3];
+
+// Set literal
+  var uniqueNumbers = {1, 2, 3};
+
+// Map literal
+  var person = {'name': 'John', 'age': 30};
+
+// Runes (UTF-32)
+  var heart = '\u2665';
+  print(heart);
+
+  print(numbers);
+  print(uniqueNumbers);
+  print(person.entries);
+   print(person);
+}
