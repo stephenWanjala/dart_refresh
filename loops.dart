ab8c950b530f41a75ebc454d24e683a2e13b326d
Wanjala Stephen <stephenwanjala145@gmail.com>
2023-05-20 14:01:21 +0300

    looops

---
+++ loops.dart
@@ -0,0 +1,24 @@
+void main(List<String> args) {
+  
+
+
+  // For loop
+for (int i = 1; i <= 5; i++) {
+  print('Number: $i');
+}
+
+// While loop
+int count = 0;
+while (count < 5) {
+  print('Count: $count');
+  count++;
+}
+
+// Do-while loop
+int num = 1;
+do {
+  print('Number: $num');
+  num++;
+} while (num <= 5);
+
+}
