44005139e261e3a1c3ebf725455c7aa5636fe28f
Wanjala Stephen <stephenwanjala145@gmail.com>
2023-05-20 15:35:46 +0300

    classes with required constructor params

---
+++ classes1.dart
@@ -0,0 +1,13 @@
+class Person {
+  String name;
+  int age;
+
+  // Constructor with required parameters
+  Person({required this.name, required this.age});
+}
+
+void main() {
+  // Creating an object with required parameters
+  var person = Person(name: 'John', age: 30);
+  print('Name: ${person.name}, Age: ${person.age}');
+}
