cddf78b1c14e59c723da06ab79065e775deb6618
Wanjala Stephen <stephenwanjala145@gmail.com>
2023-05-20 13:59:24 +0300

    conditionals & if statement

---
+++ conditionals.dart
@@ -0,0 +1,14 @@
+void main(List<String> args) {
+  
+  int a = 10;
+int b = 5;
+
+if (a > b) {
+  print('a is greater than b');
+} else if (a < b) {
+  print('a is less than b');
+} else {
+  print('a is equal to b');
+}
+
+}
