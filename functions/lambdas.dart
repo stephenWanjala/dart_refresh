caea22d2faf03e54dbee5067ea76087015b5b419
Wanjala Stephen <stephenwanjala145@gmail.com>
2023-05-20 16:38:36 +0300

    lambdas:  Assigning a Lambda to a Variabl

--- functions/lambdas.dart
+++ functions/lambdas.dart
@@ -5,4 +5,12 @@ void executeOperation(int a, int b, Function(int, int) operation) {
 
 void main() {
   executeOperation(5, 3, (a, b) => a + b);
+
+  /**
+ * 
+ * Assigning a Lambda to a Variable:
+ */
+
+  var add = (int a, int b) => a + b;
+  print("Lambda variable : ${add(5, 3)}"); // Output: 8
 }
