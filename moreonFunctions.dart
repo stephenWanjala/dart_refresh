a4f8d75b0cf567f9ac72707672ea6df78dc87012
Wanjala Stephen <stephenwanjala145@gmail.com>
2023-05-20 15:38:00 +0300

    maned aparameters

--- moreonFunctions.dart
+++ moreonFunctions.dart
@@ -5,4 +5,14 @@ void greet(String name, {String greeting = 'Hello'}) {
 void main() {
   greet('John');  // Output: Hello, John!
   greet('Sarah', greeting: 'Hi');  // Output: Hi, Sarah!
+
+  introduce(name: 'John', age: 30);
+  introduce(age: 25, name: 'Sarah');
 }
+
+
+
+void introduce({String name = '', int age = 0}) {
+  print('My name is $name, and I am $age years old.');
+}
+
