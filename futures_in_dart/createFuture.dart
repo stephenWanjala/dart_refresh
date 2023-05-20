e23e53b17eb46a0119d8faac724d2c4efc9eee6d
Wanjala Stephen <stephenwanjala145@gmail.com>
2023-05-20 16:50:50 +0300

    futures

---
+++ futures_in_dart/createFuture.dart
@@ -0,0 +1,13 @@
+Future<int> fetchData() {
+  return Future<int>.delayed(Duration(seconds: 2), () => 42);
+}
+
+void main(List<String> args) {
+  fetchData().then((value) => print(value));
+
+//or
+
+  fetchData().then((result) {
+    print(result); // Output: 42
+  });
+}
