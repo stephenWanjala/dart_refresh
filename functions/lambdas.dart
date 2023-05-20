e0fd5917f6b7e3f890e810d2137211ac6159a29c
Wanjala Stephen <stephenwanjala145@gmail.com>
2023-05-20 16:40:55 +0300

    Using Lambdas with Higher-Order Functions

--- functions/lambdas.dart
+++ functions/lambdas.dart
@@ -13,4 +13,26 @@ void main() {
 
   var add = (int a, int b) => a + b;
   print("Lambda variable : ${add(5, 3)}"); // Output: 8
+
+
+
+
+  /**
+   * 
+   * Using Lambdas with Higher-Order Functions:
+   */
+
+
+ var numbers = [1, 2, 3, 4, 5];
+
+  // Using lambda with map()
+  var squaredNumbers = numbers.map((number) => number * number);
+  print(squaredNumbers);  // Output: (1, 4, 9, 16, 25)
+
+  // Using lambda with where()
+  var evenNumbers = numbers.where((number) => number % 2 == 0);
+  print(evenNumbers);  // Output: (2, 4)
+
+
+
 }
