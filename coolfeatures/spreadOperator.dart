eeb4616aff147248cbe6f8480c4cff2f1b81ce4c
Wanjala Stephen <stephenwanjala145@gmail.com>
2023-05-20 16:01:49 +0300

    spread operator: combining & copying lists

---
+++ coolfeatures/spreadOperator.dart
@@ -0,0 +1,31 @@
+/**
+ * you can use spread operations to concatenate or
+ *  expand lists and collections. The spread operator (...) 
+ * allows you to combine multiple elements into a new list or collection
+ */
+
+
+/**
+ * Combining Lists:
+ */
+
+void main(List<String> args) {
+  var list1 = [1, 2, 3];
+var list2 = [4, 5, 6];
+var combinedList = [...list1, ...list2];
+
+print(combinedList);  // Output: [1, 2, 3, 4, 5, 6]
+
+
+/**
+ * 
+ * Creating a Copy of a List
+ */
+
+var originalList = [1, 2, 3];
+var copyList = [...originalList];
+
+print(copyList);  // Output: [1, 2, 3]
+
+
+}
