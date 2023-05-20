fdcef565186fa373093dcd2b20c14d54262e97f5
Wanjala Stephen <stephenwanjala145@gmail.com>
2023-05-20 16:56:25 +0300

    combining & chaining futures

--- futures_in_dart/createFuture.dart
+++ futures_in_dart/createFuture.dart
@@ -12,4 +12,25 @@ void main(List<String> args) {
   }).catchError((error) {
     print('Error: $error');
   });
+
+  /**
+   * chain
+   */
+
+  fetchData().then((result) {
+    return '$result processed';
+  }).then((processedResult) {
+    print(processedResult); // Output: Data processed
+  });
+
+  /**
+   * combine
+   */
+
+  var future1 = Future.delayed(Duration(seconds: 2), () => 'Hello');
+  var future2 = Future.delayed(Duration(seconds: 3), () => 'World');
+
+  Future.wait([future1, future2]).then((results) {
+    print(results); // Output: [Hello, World]
+  });
 }
